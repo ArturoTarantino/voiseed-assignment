2a8bf006ace4d8e8080b595346d34f2a3cf4f840
ArturoTarantino <arturotarantino92@gmail.com>
2024-11-05 20:03:04 +0100

    add conditional time interval

--- src/components/Waveform.tsx
+++ src/components/Waveform.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import WaveSurfer from "wavesurfer.js";
 import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.esm.js';
-import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'; // Importa il plugin Regions
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'; 
 import { useSubtitles } from "../context/SubtitleContext";
 import { getVideoFromIndexedDB } from "../utils/DBops";
 import { Subtitle } from "./SubTitleBox";
@@ -12,7 +12,6 @@ interface Props {
 }
 
 const Waveform = ({ onReady }: Props) => {
-
     const waveSurferRef = useRef<WaveSurfer | null>(null);
     const waveformContainerRef = useRef<HTMLDivElement | null>(null);
 
@@ -22,13 +21,35 @@ const Waveform = ({ onReady }: Props) => {
         getVideoFromIndexedDB().then(videoBlob => {
             if (videoBlob && waveformContainerRef.current) {
                 const url = URL.createObjectURL(videoBlob);
+                
+                const videoDuration = JSON.parse(localStorage.getItem('videoDuration') as string);
+
+                let timeInterval;
+                let primaryLabelInterval;
+                let fontsize = '18px';
+                
+                if (videoDuration <= 30) {
+                    timeInterval = 0.1;
+                    primaryLabelInterval = 1;
+                } else if(videoDuration > 30 && videoDuration <= 60) {
+                    timeInterval = 1;
+                    primaryLabelInterval =  5;
+                } else if (videoDuration >= 60 && videoDuration <= 120) {
+                    timeInterval = 5;
+                    primaryLabelInterval =  20;
+                    fontsize = '10px';
+                } else if (videoDuration > 120) {
+                    timeInterval = 10;
+                    primaryLabelInterval = 30;
+                    fontsize = '10px';
+                }
 
                 const bottomTimeline = TimelinePlugin.create({
                     height: 30,
-                    timeInterval: 0.1,
-                    primaryLabelInterval: 1,
+                    timeInterval: timeInterval,
+                    primaryLabelInterval: primaryLabelInterval,
                     style: {
-                        fontSize: '18px',
+                        fontSize: fontsize,
                         color: 'white',
                     },
                 });
@@ -39,7 +60,7 @@ const Waveform = ({ onReady }: Props) => {
                     container: waveformContainerRef.current,
                     waveColor: '#3a6ea5',
                     progressColor: '#004e98',
-                    cursorColor: '#FFF',
+                    cursorColor: '#C2B280',
                     height: 180,
                     barWidth: 0,
                     hideScrollbar: true,
@@ -62,23 +83,24 @@ const Waveform = ({ onReady }: Props) => {
                     }
                 });
 
-                
                 waveSurferRef.current.on('decode', () => {
                     const subtitles = JSON.parse(localStorage.getItem('subtitles') as string);
                     subtitles.forEach((subtitle: Subtitle, index: number) => {
                         regions.addRegion({
                             start: timeToSeconds(subtitle.start),
+                            end: timeToSeconds(subtitle.end),
                             content: `sub #${index + 1}`,
-                            color: 'rgba(200, 200, 200, 0.4)',
+                            color: 'rgba(200, 200, 200, 0.2)',
                             drag: false,
                             resize: false
                         });
-                        if(index === subtitles.length - 1) {
-                            regions.addRegion({
-                                start: timeToSeconds(subtitle.end),
-                                color: 'rgba(200, 200, 200, 0.4)',
-                            });
-                        }
+                        regions.addRegion({
+                            start: timeToSeconds(subtitle.end),
+                            content: ``,
+                            color: 'rgba(200, 200, 200, 0.2)',
+                            drag: false,
+                            resize: false
+                        });
                     });
                 });
 
